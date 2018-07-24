1c05befab90cc6673237596d69fccdefbf19f77d
JasmineLi <jasmine_chieh_li@outlook.com>
2018-07-24 18:09:52 +0800

    little convenience

---
+++ li.sh
@@ -0,0 +1,5 @@
+dir=`date +%y%m`
+file="`date +%d`li.md"
+newFile="${dir}/${file}"
+
+vim ${newFile}
---
+++ reSummary.sh
@@ -0,0 +1,23 @@
+#您要是不嫌弃我的代码，第三行cd改一下，li都改成sue就能用了
+
+cd /e/see-u-in-fall
+
+cur=`date +%y%m`  #1807
+curFile="`date +%d`li.md"  #24li.md
+curPath="${cur}/${curFile}"  #1807/24li.md
+
+preLine=$(tail -1 SUMMARY.md)
+temp=${preLine##*(}  #1807/23sue.md)
+prePath=${temp%)*}  #1807/23sue.md
+
+read line < $curPath
+title=${line:2}
+
+if [ "$curPath"x = "$prePath"x ]
+then
+    sed -i '$d' SUMMARY.md
+fi
+
+day=`date +%d`
+echo -e "     * [${day} ${title}](${curPath})" >> SUMMARY.md
+cat SUMMARY.md
